d8c655bfeb7923ce5914b841fe65f752a92a67b2
Kyle Yeung <kyleyeung000@gmail.com>
2022-06-16 23:47:53 +0800

    Reworked to align with ERC20 token standard

--- contracts/VNNN.sol
+++ contracts/VNNN.sol
@@ -1,17 +1,20 @@
 pragma solidity >=0.8.0 <0.9.0;
 
-import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./Authorizable.sol";
 
-contract VNNN20 is ERC20, ERC20Permit, Authorizable {
-    
-    constructor() ERC20("VNNN ERC20", "VNNN20") ERC20Permit("VNNN20") {
-        _mint(msg.sender, 100000000 * 10 ** decimals());
+contract VNNN is ERC20, Authorizable {
+
+    constructor() ERC20("VNNN", "VNNN") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
     }
 
     function mint(address to, uint256 amount) public onlyAuthorized {
         _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public {
+        _burn(from, amount);
     }
 
     function decimals() public view virtual override returns (uint8) {
