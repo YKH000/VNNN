7f5bdf035858ebdfbb10b04b904a84075c47aa59
YKH000 <44135630+YKH000@users.noreply.github.com>
2022-06-25 00:28:42 +0800

    Revert "24 jun2022 x13"

--- test/VNNNTest1.js
+++ test/VNNNTest1.js
@@ -2,48 +2,27 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("VNNN Test 1", function () {
-  let owner;
-  let addr1;
-  let addr2;
-  let addrs;
-  let VNNN;
-  let vnnn;
-
-  beforeEach(async function () {
-    VNNN = await ethers.getContractFactory("VNNN");
-    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
-
-    vnnn = await VNNN.deploy();
+  it("Should return the total supply of tokens once deployed as 1 million times 10^4", async function () {
+    const VNNN = await ethers.getContractFactory("VNNN");
+    const vnnn = await VNNN.deploy();
     await vnnn.deployed();
+
+    expect(await vnnn.totalSupply()).to.equal(10000000000);
   });
 
-  describe("Owner Details", function () {
-    it("Should set the correct owner", async function () {
-      expect(await vnnn.owner()).to.equal(owner.address);
-    });
+  it("Should return the Decimals as 4 once deployed", async function () {
+    const VNNN = await ethers.getContractFactory("VNNN");
+    const vnnn = await VNNN.deploy();
+    await vnnn.deployed();
 
-    it("Should assign initial token supply to owner", async function () {
-      expect(await vnnn.totalSupply()).to.equal(
-        await vnnn.balanceOf(owner.address)
-      );
-    });
+    expect(await vnnn.decimals()).to.equal(4);
   });
 
-  describe("Total Supply and Decimals", function () {
-    it("Should return the total supply of tokens once deployed as 1 million times 10^4", async function () {
-      expect(await vnnn.totalSupply()).to.equal(10000000000);
-    });
+  it("Should return 50000 as the output of intAmount(5) once deployed", async function () {
+    const VNNN = await ethers.getContractFactory("VNNN");
+    const vnnn = await VNNN.deploy();
+    await vnnn.deployed();
 
-    it("Should return the Decimals as 4 once deployed", async function () {
-      expect(await vnnn.decimals()).to.equal(4);
-    });
-
-    it("Should return 50000 as the output of intAmount(5) once deployed", async function () {
-      expect(await vnnn.intAmount(5)).to.equal(50000);
-    });
-  });
-
-  describe("Transactions", function () {
-    //Placeholder for tests relating to transactions
+    expect(await vnnn.intAmount(5)).to.equal(50000);
   });
 });
